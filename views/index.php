f1f92e2aca437fed26babde79e054c62db8e9c3e
Virginie Dourson <virginiedourson@gmail.com>
2023-11-20 11:48:59 +0100

    Header

--- views/index.php
+++ views/index.php
@@ -2,19 +2,7 @@
 $title = "Home";
 require_once __DIR__ . '/partials/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokedex</title>
-    <link rel="stylesheet" href="dist/style.css" type="text/css">
-</head>
-</head>
-<body>
-    
-</body>
-</html>
+
 <main class="w-full">
     <h1>Pokedex - Homepage</h1>
     <p>Hello <strong><?php echo $user['name'] ?></p></strong>
@@ -33,6 +21,8 @@ require_once __DIR__ . '/partials/header.php';
     </div>
     <a href="/Pokedex/index.php/pokemon?name=pickachu">Pikachu</a>
 </main>
+
+
 
 <?php
 require_once __DIR__ . '/partials/footer.php';
--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title;?> - Pokedex</title>
-    <link href="../../public/css/styles.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/style.css" type="text/css">
 </head>
 <body>
+    <header class="flex flex-col items-center gap-4 p-y-5 border-y-2">
+        <img class="" src="public/img/pokemon-logo.png" width="250" alt="Logo pokémon">
+        <nav class="w-full flex justify-center gap-4 border-y-2 border-slate-400 py-2">
+            <a class="" href="/Pokedex/">Pokemon</a>
+            <a href="/Pokedex/index.php/">My account</a>
+            <a href="">Register</a>
+        </nav>
+    </header>
     
